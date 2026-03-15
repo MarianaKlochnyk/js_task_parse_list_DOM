1331c4e0e3281e157ae72580497d1f7b511cd5f8
Mariana Klochnyk <klochnykmaryana@gmail.com>
2026-03-15 21:39:30 +0200

    edit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('ul');
+
+function getSalary(li) {
+  const salary = li.dataset.salary;
+  const num = Number(salary.replace(/[$,]/g, ''));
+
+  return num;
+}
+
+function sortList(li) {
+  const items = Array.from(li.querySelectorAll('li'));
+
+  items.sort((a, b) => getSalary(b) - getSalary(a));
+  items.forEach((item) => li.appendChild(item));
+}
+
+function getEmployees(lis) {
+  return Array.from(lis.querySelectorAll('li')).map((li) => ({
+    name: lis.textContent,
+    salary: getSalary(lis),
+    position: lis.dataset.position,
+    age: Number(lis.dataset.age),
+  }));
+}
+
+sortList(list);
+getEmployees(list);
